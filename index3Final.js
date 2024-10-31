b9e972401eac814c83bae655560cf3298168568e
morty-9 <hzawnineseven@gmail.com>
2024-10-31 08:50:00 +0630

    4

--- index3Final.js
+++ index3Final.js
@@ -2,13 +2,19 @@ var gcdOfStrings = function(str1, str2) {
   function gcd(a, b) {
     return b === 0 ? a : gcd(b, a % b);
   }
+  const gcdLength = gcd(str1.length, str2.length); 6
+  const candidate = str1.slice(0, gcdLength); 
   function isConstructedFrom(str, pattern) {
     return pattern.repeat(str.length / pattern.length) === str;
   }
-  const gcdLength = gcd(str1.length, str2.length);
-  const candidate = str1.slice(0, gcdLength);
   if (isConstructedFrom(str1, candidate) && isConstructedFrom(str2, candidate)) {
     return candidate;
   }
   return "";
 };
+console.log(gcdOfStrings( "abcdef" ,"abcdefabcdefabcdef" ));
+// 6 , 18
+
+// 18, 6
+
+// 6 , 0
